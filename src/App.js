bd346e4b3c8ce78afecdaa92275c355f7939794f
lealinin <lea.linin@gmail.com>
2020-03-16 15:33:10 -0700

    Add axios API call to grab quote

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -9,7 +9,14 @@ class App extends Component {
   }
 
   componentDidMount() {
-    console.log('componentDidMount runs')
+    this.getQuote();
+  }
+
+  getQuote() {
+    let url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
+    axios.get(url)
+      .then(res => console.log(res))
   }
 
   render() {
