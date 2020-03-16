4f075f9c7a493330af18d5eaedfd0ad2a37e8325
lealinin <lea.linin@gmail.com>
2020-03-16 16:00:12 -0700

    Add RandomQuote component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import RandomQuote from './RandomQuote';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    console.log('constructor runs')
-  }
-
-  componentDidMount() {
-    this.getQuote();
-  }
-
-  getQuote() {
-    let url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
-
-    axios.get(url)
-      .then(res => console.log(res))
-  }
 
   render() {
     console.log('render method runs')
     return(
       <div>
         <h1>Hello</h1>
+        <RandomQuote />
       </div>
     );
   }
