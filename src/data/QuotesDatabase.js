d0f1eb4790c72498ca728b8915818ed9ebf3f70a
lealinin <lea.linin@gmail.com>
2020-03-17 13:02:20 -0700

    Add array of quote-author objects

---
+++ src/data/QuotesDatabase.js
@@ -0,0 +1,109 @@
+const quotes=[
+	{
+    "quote": "Did you ever stop to think, and forget to start again?",
+    "author": "A. A. Milne"
+    }, {
+    "quote": "No act of kindness, no matter how small, is ever wasted.",
+    "author": "Aesop"
+    }, {
+    "quote": "Just living is not enough…one must have sunshine, freedom and a little flower.",
+    "author": "Hans Christian Andersen"
+    }, {
+    "quote": "One of the advantages of being disorderly is that one is constantly making exciting discoveries.",
+    "author": "A. A. Milne"
+    }, {
+    "quote": "Adventure is worthwhile.",
+    "author": "Aesop"
+    }, {
+    "quote": "Look deep into nature, and then you will understand everything better.",
+    "author": "Albert Einstein"
+    }, {
+    "quote": "Nothing happens unless first we dream.",
+    "author": "Carl Sandburg"
+    }, {
+    "quote": "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+    "author": "Albert Einstein"
+    }, {
+    "quote": "I have no special talent. I am only passionately curious.",
+    "author": "Albert Einstein"
+    }, {
+    "quote": "A table, a chair, a bowl of fruit and a violin; what else does a man need to be happy",
+    "author": "Albert Einstein"
+    }, {
+    "quote": "Peace comes from within. Do not seek it without.",
+    "author": "Buddha"
+    }, {
+    "quote": "Coincidence is God’s way of remaining anonymous.",
+    "author": "Albert Einstein"
+    }, {
+    "quote": "It takes courage to grow up and become who you really are.",
+    "author": "e.e. cummings"
+    }, {
+    "quote": "Listen; there’s a hell of a good universe next door: let’s go.",
+    "author": "e.e. cummings"
+    }, {
+    "quote": "I’ve never known any trouble that an hour’s reading didn’t assuage.",
+    "author": "Arthur Schopenhauer"
+    }, {
+    "quote": "I dwell in possibility.",
+    "author": "Emily Dickinson"
+    }, {
+    "quote": "Forever is composed of nows.",
+    "author": "Emily Dickinson"
+    }, {
+    "quote": "It is never too late to be what you might have been.",
+    "author": "George Eliot"
+    }, {
+    "quote": "The real voyage of discovery consists not in seeking new landscapes, but in having new eyes.",
+    "author": "Marcel Proust"
+    }, {
+    "quote": "We don’t receive wisdom; we must discover it for ourselves after a journey that no one can take for us or spare us.",
+    "author": "Marcel Proust"
+    }, {
+    "quote": "If a little dreaming is dangerous, the cure for it is not to dream less but to dream more, to dream all the time.",
+    "author": "Marcel Proust"
+    }, {
+    "quote": "We must never be afraid to go too far, for truth lies beyond.",
+    "author": "Marcel Proust"
+    }, {
+    "quote": "A happy life must be to a great extent a quiet life, for it is only in an atmosphere of quiet that true joy dare live.",
+    "author": "Bertrand Russell"
+    }, {
+    "quote": "We shall not cease from exploration, and the end of all our exploring will be to arrive where we started and know the place for the first time.",
+    "author": "T.S. Eliot"
+    }, {
+    "quote": "Doing nothing is better than being busy doing nothing.",
+    "author": "Lao Tzu"
+    }, {
+    "quote": "A leaf fluttered in through the window this morning, as if supported by the rays of the sun, a bird settled on the fire escape, joy in the task of coffee, joy accompanied me as I walked.",
+    "author": "Anaïs Nin"
+    }, {
+    "quote": "Things won are done, joy’s soul lies in the doing.",
+    "author": "William Shakespeare"
+    }, {
+    "quote": "I like cappuccino, actually. But even a bad cup of coffee is better than no coffee at all.",
+    "author": "David Lynch"
+    }, {
+    "quote": "It is a sad and beautiful world.",
+    "author": "Jim Jarmusch"
+    },{
+    "quote": "All that is gold does not glitter; not all those who wander are lost; the old that is strong does not wither; deep roots are not reached by the frost.",
+    "author": "J. R. R. Tolkien"
+    }, {
+    "quote": "I have discovered in life that there are ways of getting almost anywhere you want to go, if you really want to go.",
+    "author": "Langston Hughes"
+    }, {
+    "quote": "Be as you wish to seem.",
+    "author": "Socrates"
+    }, {
+    "quote": "Do, or do not. There is no try.",
+    "author": "Yoda"
+    }, {
+    "quote": "Wisdom begins in wonder.",
+    "author": "Socrates"
+    }, {
+    "quote": "I never think of the future. It comes soon enough.",
+    "author": "Albert Einstein"
+  }, 
+]
+export default quotes
