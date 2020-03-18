3342d77d1f4c9f6ed4741832d507c629df7a2412
lealinin <lea.linin@gmail.com>
2020-03-18 12:29:56 -0700

    Change button name

--- src/QuoteAndAuthor.js
+++ src/QuoteAndAuthor.js
@@ -1,30 +1,36 @@
 import React, { Component } from 'react';
+import './QuoteBox.css';
 
 class QuoteAndAuthor extends Component {
   render() {
-    const randomColor = this.props.displayColor;
+    // const randomColor = this.props.displayColor;
     // const randomColor = this.props.displayColor();
-    const html = document.documentElement;
-    html.style.backgroundColor = randomColor;
+    // const html = document.documentElement;
+    // html.style.backgroundColor = randomColor;
 
     return (
-      <div style={{ backgroundColor: "white"}} className="quotebox">
-        <div
-          className="fadeIn"
-          key={Math.random()}
-          style={{ color: randomColor }}
-        >
-          <h1 id="quote">{this.props.quote}</h1>
-          <h5 id="author">{this.props.author}</h5>
+      <>
+        <div className="QuoteBox">
+          <div
+            className="fadeIn"
+            key={Math.random()}
+          // style={{ color: randomColor }}
+          >
+            <h2 id="quote">{this.props.quote}</h2>
+            <h4 id="author">{this.props.author}</h4>
+          </div>
         </div>
-      <button
-        style={{ backgroundColor: randomColor }}
-        id="newquote"
-        onClick={this.props.handleClick}
-      >
-        New Quote
+        <div className="ButtonWrapper">
+          <button
+            className="QuoteButton"
+            // style={{ backgroundColor: randomColor }}
+            id="newquote"
+            onClick={this.props.handleClick}
+          >
+            Pick Me Up
       </button>
-    </div>
+        </div>
+      </>
     );
   }
 }
